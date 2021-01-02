3a6e57a6fd9fcd64294c16c67a9b94c133ae68f3
Jason Ling <jason.ling5242@gmail.com>
2021-01-02 13:14:21 -0500

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './App.css';
-import React, {useState} from "react";
-import {useForm} from './useForm';
+import React, { useEffect, useState } from "react";
+import { useForm } from './useForm';
+import {Hello} from './Hello';
+import { useFetch } from './useFetch';
 
 const App = () => {
+  const [values, handleChange] = useForm({email: '', password: '', firstName: ''});
 
-  const [values, handleChange] = useForm({email: '', password: ''})
+  const [showHello, setShowHello] = useState(true);
+
+  const [count, setCount] = useState(0);
+  const {data, loading} = useFetch(`http://numbersapi.com/${count}/trivia`);
+
   return (
   <div>
+    <div>{!data ? 'loading...' : data}</div>
+    <div>count: {count}</div>
+    <button onClick={() => setCount(c => c+1)}>increment</button>
+    {/* <button onClick={()=> setShowHello(!showHello)}>toggle</button>
+    {showHello && <Hello />} */}
     <input name='email' value={values.email} onChange={handleChange} />
+    <input placeholder="firstname" name='firstName' value={values.firstName} onChange={handleChange}/>
     <input type="password" name='password' value={values.password} onChange={handleChange}></input>
   </div>
   );
