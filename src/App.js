0395b29cd14cca630d8c1a679491c29703962b4f
Jason Ling <jason.ling5242@gmail.com>
2021-01-02 13:22:42 -0500

    localStorage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import React, { useEffect, useState } from "react";
 import { useForm } from './useForm';
-import {Hello} from './Hello';
 import { useFetch } from './useFetch';
 
 const App = () => {
@@ -9,8 +8,12 @@ const App = () => {
 
   const [showHello, setShowHello] = useState(true);
 
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(() => JSON.parse(localStorage.getItem('count')));
   const {data, loading} = useFetch(`http://numbersapi.com/${count}/trivia`);
+
+  useEffect(() => {
+    localStorage.setItem('count', JSON.stringify(count))
+  },[count])
 
   return (
   <div>
