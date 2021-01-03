48c2f76d0fa14f87dea7f2a38d76ab9490afa9c6
Jason Ling <jason.ling5242@gmail.com>
2021-01-03 12:10:26 -0500

    useRef

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
 import './App.css';
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useForm } from './useForm';
 import { useFetch } from './useFetch';
+import { Hello } from './Hello';
 
 const App = () => {
   const [values, handleChange] = useForm({email: '', password: '', firstName: ''});
 
   const [showHello, setShowHello] = useState(true);
 
-  const [count, setCount] = useState(() => JSON.parse(localStorage.getItem('count')));
-  const {data, loading} = useFetch(`http://numbersapi.com/${count}/trivia`);
+  const inputRef = useRef();
 
-  useEffect(() => {
-    localStorage.setItem('count', JSON.stringify(count))
-  },[count])
 
   return (
   <div>
-    <div>{!data ? 'loading...' : data}</div>
-    <div>count: {count}</div>
-    <button onClick={() => setCount(c => c+1)}>increment</button>
-    {/* <button onClick={()=> setShowHello(!showHello)}>toggle</button>
-    {showHello && <Hello />} */}
-    <input name='email' value={values.email} onChange={handleChange} />
+    <>
+    <button onClick={()=> setShowHello(!showHello)}>toggle</button>
+    {showHello && <Hello />}
+    <input ref={inputRef} name='email' value={values.email} onChange={handleChange} />
     <input placeholder="firstname" name='firstName' value={values.firstName} onChange={handleChange}/>
     <input type="password" name='password' value={values.password} onChange={handleChange}></input>
+    <button onClick={()=>{
+      inputRef.current.focus();
+    }}>focus</button>
+    </>
   </div>
   );
 };
--- src/Hello.js
+++ src/Hello.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, {useRef, useState, useEffect} from 'react';
+import { useFetch } from './useFetch';
 
 export const Hello = () => {
-    React.useEffect(() => {
-        console.log("render");
-    
-        return () => {
-          console.log("unmount");
-        }
-    
-      }, [])
+    //const renders = useRef(0);
+    const [count, setCount] = useState(() => JSON.parse(localStorage.getItem('count')));
+    const {data, loading} = useFetch(`http://numbersapi.com/${count}/trivia`);
+  
+    useEffect(() => {
+      localStorage.setItem('count', JSON.stringify(count))
+    },[count])
 
-    return <div>Hello</div>
+    return (
+    <div>    
+      <div>{!data ? 'loading...' : data}</div>
+      <div>count: {count}</div>
+    <button onClick={() => setCount(c => c+1)}>increment</button>
+    </div>)
 }
